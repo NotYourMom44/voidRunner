15bc725da6b874c1621f6886d9f2ac1abcf841a1
Sihle Ribisi <128409229+NotYourMom44@users.noreply.github.com>
2026-05-13 15:51:12 +0200

    Project balancing
    project balancing

--- Assets/Scenes/Scripts/pickUpSpawn.cs
+++ Assets/Scenes/Scripts/pickUpSpawn.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.EventSystems.EventTrigger;
 //using Random = System.Random;
 
 public class pickUpSpawn : MonoBehaviour
